49bec6c23a508ce8e44b99f87a67cceb7f37469a
Janelin126 <79820235+Janelin126@users.noreply.github.com>
2022-08-15 16:38:23 +1200

    workshop wk 5

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # 212-Burgers
  Flask-based website
+
+
+
+**** Mac Users *****
+cd *DRAG PROJECT FOLDER INTO TERMINAL*
+source env/bin/activate
+export FLASK_APP=run.py
+export FLASK_DEBUG=1
+flask run
+
+****Windows Users******
+cd *DRAG PROJECT FOLDER INTO TERMINAL*
+env\Scripts\activate.bat
+set FLASK_APP=run.py
+set FLASK_DEBUG=1
+py -m flask run
--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request, session, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = '123456789'
 
 MENUDB = 'menu.db'
 
@@ -93,6 +94,33 @@ def order():
 
     return render_template('order.html', burgers=menu['burgers'], drinks=menu['drinks'], sides=menu['sides'])
 
-@app.route('/confirm')
+@app.route('/confirm', methods=['post'])
 def confirm():
-    return render_template('confirm.html')
+    details = {}
+    items ={}
+
+    for input in request.form:
+         if input == 'name' or input == 'address':
+             details[input] =request.form[input]
+         elif request.form[input] and request.form[input] != '0':
+             items[input] = request.form[input]
+
+    con = sqlite3.connect(MENUDB)
+    cur = con.execute(
+      'INSERT INTO orders(name, address, items) VALUES(?, ?, ?)',
+      (details['name'], details['address'], str(items))
+    )
+    con.commit()
+    con.close()
+    
+    print(request.form)
+
+    return render_template('confirm.html', details=details, items=items)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and request.form['username'] == 'admin':
+        session['username'] = request.form['username']
+        return redirect(url_for('panel'))
+    else:
+        return render_template('login.html')
